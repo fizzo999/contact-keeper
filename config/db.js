8d30847516a2f4f3e65198fa90545290289f29f8
FIZZO PANNOSCH <fizzo999@gmail.com>
2022-12-21 09:37:14 -0800

    change db to async await and try catch

--- config/db.js
+++ config/db.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose'); //
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  mongoose
-    .set('strictQuery', false)
-    .connect(db, {})
-    .then(() => console.log('mongoDB connected'))
-    .catch(err => {
-      console.error(err.message);
-      process.exit(1);
-    });
+const connectDB = async () => {
+  try {
+    await mongoose.set('strictQuery', false).connect(db, {});
+    console.log('mongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
