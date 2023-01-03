5e64c2d63a7f24aa638410c801a987040b33e070
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-01-02 18:35:42 -0800

    adds BrowserRouter,Routes,Route and Home, About elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/layout/Home.js';
+import About from './components/layout/About.js';
 import Navbar from './components/layout/Navbar.js';
 import './App.css';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Navbar />
-      my app
-    </div>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </Fragment>
+    </BrowserRouter>
   );
 };
 
