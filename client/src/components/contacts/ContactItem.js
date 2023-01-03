a2456bbc19cf40145701c2ca4d5283a426d31b55
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-01-02 20:15:21 -0800

    map function to display list of contacts through ContactItem through use of global Context

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ContactItem = ({ contactprop }) => {
+  const { id, name, phone, email, type } = contactprop;
+  return (
+    <div
+      className='card bg-light'
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
+    >
+      <h3 className='text-primary' style={{}}>
+        {name}
+        {'  '}
+      </h3>
+      <h3
+        className={
+          'p-2' +
+          ' ' +
+          'badge' +
+          ' ' +
+          (type === 'professional' ? 'badge-success' : 'badge-primary')
+        }
+      >
+        {type}
+      </h3>
+    </div>
+  );
+};
+
+export default ContactItem;
--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext } from 'react';
 import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem.js';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
@@ -9,7 +10,7 @@ const Contacts = () => {
   return (
     <Fragment>
       {contacts.map(contact => (
-        <h3>{contact.name}</h3>
+        <ContactItem key={contact.id} contactprop={contact} />
       ))}
     </Fragment>
   );
