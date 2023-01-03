64e6c9701362a232cc45b676ed64f5a109699df8
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-01-02 19:40:09 -0800

    setting up state, reducer and context for Contacts

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home.js';
 import About from './components/pages/About.js';
 import Navbar from './components/layout/Navbar.js';
+
+import ContactState from './context/contact/ContactState.js';
 import './App.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Fragment>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />} />
-          </Routes>
-        </div>
-      </Fragment>
-    </BrowserRouter>
+    <ContactState>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/about' element={<About />} />
+            </Routes>
+          </div>
+        </Fragment>
+      </BrowserRouter>
+    </ContactState>
   );
 };
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Contacts from '../contacts/Contacts.js';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='grid-2'>
+      <div>{/* Contact Form will go here */}</div>
+      <div>
+        <Contacts />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
