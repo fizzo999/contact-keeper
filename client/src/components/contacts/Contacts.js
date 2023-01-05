70d19b847e91dc652604d37a7109938deb7123b4
FIZZO PANNOSCH <fizzo999@gmail.com>
2023-01-05 10:37:08 -0800

    fade css effect

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,17 +1,40 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem.js';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact...</h4>;
+  }
 
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem key={contact.id} contactprop={contact} />
-      ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(eachContact => (
+              <CSSTransition
+                key={eachContact.id}
+                timeout={500}
+                classNames='item'
+              >
+                <ContactItem contactprop={eachContact} />
+              </CSSTransition>
+            ))
+          : contacts.map(eachContact => (
+              <CSSTransition
+                key={eachContact.id}
+                timeout={500}
+                classNames='item'
+              >
+                <ContactItem contactprop={eachContact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
